e7babee8dc000af81e9738fac57e56c6b161b184
Josh Cook <joshuacook0023@gmail.com>
2022-12-01 08:44:45 -0800

    feat: implement `aoc` command to take option day argument

--- advent_of_code/__init__.py
+++ advent_of_code/__init__.py
@@ -1,10 +1,39 @@
 """Advent of Code 2022"""
 
+from typing import Callable, Final
+
+from typer import Typer
+
 from advent_of_code.puzzles import day01
+
+app = Typer()
 
 __version__ = "1.0.0"
 
+PUZZLES: Final[dict[int, Callable[[], None]]] = {
+    1: day01.main,
+}
 
-def main() -> None:
+
+def _run_all_puzzles() -> None:
+    for fxn in PUZZLES.values():
+        fxn()
+
+
+def _run_puzzle(day: int) -> None:
+    PUZZLES[day]()
+
+
+@app.command()
+def run_puzzles(day: int | None = None) -> None:
+    """Run puzzles.
+
+    Args:
+        day (int | None, optional): Specific day to run the puzzles for. Defaults to
+        `None` to run all puzzles.
+    """
     print("--- Advent of Code 2022 ---")
-    day01.main()
+    if day is None:
+        _run_all_puzzles()
+    else:
+        _run_puzzle(day=day)
