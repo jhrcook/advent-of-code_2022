0a0c52b73b0facac82a1996c871968a977ac1790
Josh Cook <joshuacook0023@gmail.com>
2022-12-09 11:15:17 -0800

    puzzle: day 9 (day 2)

--- advent_of_code/puzzles/day09.py
+++ advent_of_code/puzzles/day09.py
@@ -1,5 +1,7 @@
 """Advent of Code 2022 – Day 9. Rope Bridge."""
 
+from itertools import product
+from math import sqrt
 from typing import Final
 
 from advent_of_code.checks import check_result
@@ -54,45 +56,60 @@ Position = tuple[int, int]
 class Rope:
     """Model of this short rope."""
 
-    def __init__(self) -> None:
-        self.head = (0, 0)
-        self.tail = (0, 0)
+    def __init__(self, length: int) -> None:
+        self.length: int = length
+        self.knots: list[Position] = [(0, 0) for _ in range(length)]
 
     def __str__(self) -> str:
-        return f"H: {self.head}  T: {self.tail}"
+        return " - ".join([str(x) for x in self.knots])
 
     def __repr__(self) -> str:
         return str(self)
 
-    def move(self, instruction: MoveInstruction) -> list[tuple[Position, Position]]:
+    def move(self, instruction: MoveInstruction) -> list[list[Position]]:
         """Move the rope following some instruction."""
-        positions: list[tuple[Position, Position]] = []
-        m = -1 if instruction.direction in ("D", "L") else 1
+        positions: list[list[Position]] = []
         for _ in range(instruction.steps):
-            if instruction.direction in ("U", "D"):
-                self.head = self.head[0] + m, self.head[1]
-                if (
-                    abs(self.tail[0] - self.head[0]) < 2
-                    and abs(self.tail[1] - self.head[1]) < 2
-                ):
-                    ...  # do nothing
-                elif self.head[1] == self.tail[1]:
-                    self.tail = self.tail[0] + m, self.tail[1]
+            new_knots: list[Position] = []
+            for i, knot in enumerate(self.knots):
+                if i == 0:
+                    knot = self._move_head(knot, instruction=instruction)
                 else:
-                    self.tail = self.tail[0] + m, self.head[1]
-            else:
-                self.head = self.head[0], self.head[1] + m
-                if (
-                    abs(self.tail[0] - self.head[0]) < 2
-                    and abs(self.tail[1] - self.head[1]) < 2
-                ):
-                    ...
-                elif self.head[0] == self.tail[0]:
-                    self.tail = self.tail[0], self.tail[1] + m
-                else:
-                    self.tail = self.head[0], self.tail[1] + m
-            positions.append((self.head, self.tail))
+                    knot = self._move_trailing_knot(a=new_knots[-1], b=knot)
+                new_knots.append(knot)
+            self.knots = new_knots
+            positions.append(new_knots)
         return positions
+
+    def _move_head(self, head: Position, instruction: MoveInstruction) -> Position:
+        m = -1 if instruction.direction in ("D", "L") else 1
+        if instruction.direction in ("U", "D"):
+            return (head[0] + m, head[1])
+        else:
+            return (head[0], head[1] + m)
+
+    def _move_trailing_knot(self, a: Position, b: Position) -> Position:
+        if abs(b[0] - a[0]) < 2 and abs(b[1] - a[1]) < 2:
+            return b  # No movement.
+        return find_closest_position_for_b(a, b)
+
+
+def distance(a: Position, b: Position) -> float:
+    """Calculate the distance between two points."""
+    return sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+
+def find_closest_position_for_b(target: Position, b: Position) -> Position:
+    """Find the closest position for point `b` to `target`."""
+    new_pos: Position = b
+    min_dist: float = 5.0
+    for r, c in product(range(-1, 2), range(-1, 2)):
+        pos = (b[0] + r, b[1] + c)
+        d = distance(target, pos)
+        if d < min_dist:
+            new_pos = pos
+            min_dist = d
+    return new_pos
 
 
 def parse_move_instructions(instruct_str: str) -> list[MoveInstruction]:
@@ -102,17 +119,22 @@ def parse_move_instructions(instruct_str: str) -> list[MoveInstruction]:
 
 def puzzle_1(instructions: list[MoveInstruction]) -> int:
     """Puzzle 1."""
-    rope = Rope()
+    rope = Rope(2)
     tail_positions: set[Position] = set()
     for instruction in instructions:
         positions = rope.move(instruction)
-        tail_positions = tail_positions.union([p[1] for p in positions])
+        tail_positions = tail_positions.union([p[-1] for p in positions])
     return len(tail_positions)
 
 
-def puzzle_2() -> None:
+def puzzle_2(instructions: list[MoveInstruction]) -> int:
     """Puzzle 2."""
-    ...
+    rope = Rope(10)
+    tail_positions: set[Position] = set()
+    for instruction in instructions:
+        positions = rope.move(instruction)
+        tail_positions = tail_positions.union([p[-1] for p in positions])
+    return len(tail_positions)
 
 
 def main() -> None:
@@ -124,9 +146,14 @@ def main() -> None:
     check_result(6332, res1)
 
     # Puzzle 2.
-    ...
+    ex_res = puzzle_2(parse_move_instructions(example_input))
+    check_result(1, ex_res)
+    ex_res = puzzle_2(parse_move_instructions(example_input2))
+    check_result(36, ex_res)
+    res2 = puzzle_2(parse_move_instructions(read_input_to_string(DAY)))
+    check_result(2511, res2)
 
-    print_results(DAY, TITLE, result1=res1, result2=None)
+    print_results(DAY, TITLE, result1=res1, result2=res2)
 
 
 if __name__ == "__main__":
